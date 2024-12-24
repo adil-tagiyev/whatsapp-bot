1e560310e2222fe349881d62c1d9d7c38cd3d157
adil-tagiyev <adiltagiyev23@gmail.com>
2024-12-24 05:58:22 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2020 Yusuf Usta.
+/(/* Copyright (C) 2020 Yusuf Usta.
 
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
@@ -6,9 +6,9 @@ you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
 */
 
-const { Sequelize } = require("sequelize")
+const { Sequelize } = require("levanter_2461e545767da8433eb12fd722e5d55668)
 const fs = require("fs")
-if (fs.existsSync("config.env"))
+if (fs.existsSync("config.env")
   require("dotenv").config({ path: "./config.env" })
 
 // Özel Fonksiyonlarımız
